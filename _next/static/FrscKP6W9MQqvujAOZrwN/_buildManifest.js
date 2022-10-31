f8fa8560cd9fdff57aecc64497459c535eceaf59
behnamrhp <74137947+behnamrhp@users.noreply.github.com>
2022-10-31 13:00:34 +0000

    Deploying to gh-pages from @ SmartCompiler/smartcompiler.github.io@44711ca428794913d811009ea5728fccaccb20de 🚀

---
+++ _next/static/FrscKP6W9MQqvujAOZrwN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4121b4253988251c.js"],"/_error":["static/chunks/pages/_error-2368f2bbbb3169cf.js"],"/roadmap":["static/chunks/pages/roadmap-935fb16612659daa.js"],sortedPages:["/","/_app","/_error","/roadmap"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
