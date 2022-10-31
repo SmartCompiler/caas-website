e3863dacbbd2b8adf42c80d438526eff1496b439
behnam rahimpour <74137947+behnamrhp@users.noreply.github.com>
2022-10-31 15:21:59 +0330

    [REF]: remove subdirectories

--- next.config.js
+++ next.config.js
@@ -7,9 +7,7 @@ const nextConfig = {
   images:{
     loader: 'akamai',
     path: ''
-  },
-  basePath: environment === 'production' ? process.env.NEXT_PUBLIC_SUB_DIR : '',
-  assetPrefix: environment === 'production' ? process.env.NEXT_PUBLIC_SUB_DIR : '',
+  }
 }
 
 module.exports = nextConfig
